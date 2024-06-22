cd4a934b1222ec98627348437fccc2f2788bbd81
Eesha Niaz <eshaniaz5@gmail.com>
2024-06-22 23:02:11 +0500

    class16

--- class16_FrontendAPI-Integration/my-app/app/page.tsx
+++ class16_FrontendAPI-Integration/my-app/app/page.tsx
@@ -25,13 +25,17 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Todo app</h1>
       
-      <button onClick={getTodo} className="rounded p-3 bg-amber-400 text-center">Get All Todos </button>
+
+      <button onClick={getTodo} className="rounded p-3 my-4 bg-amber-400 text-center">Get All Todos </button>
 
       {todos.map((todo) => {
         return   <div>
           {todo.id} - {todo.todo}
         </div>
       })}
+      {/* // eslint-disable-next-line @next/next/no-img-element */}
+      {/* this image is related to class 17 => The url is getting by upload the local image file in the firebase console => built => storage */}
+      <img  src="https://firebasestorage.googleapis.com/v0/b/esha-3921e.appspot.com/o/image3.png?alt=media&token=e7ff93ba-20d5-460a-980c-7ecdab743382" alt="" />
     </main>
   );
 }
